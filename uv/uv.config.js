b0273715f2198278c723b1fc0ed2033e6873b917
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-03-04 09:39:59 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://tomp.app',
+    bare: '/main/uv/uv.config.js',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
